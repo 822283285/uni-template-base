943daec1f4f5ccc34fe37d855c02b900d7bb7804
我家猫会后空翻 <2194292088@qq.com>
2025-06-13 18:48:48 +0800

    feat(utils): 添加时间常量并导出$constant
    添加了时间相关的常量定义，包括秒、分钟、小时、天、月和年等不同时间单位的毫秒值。同时将$constant导出以便其他模块使用。

--- src/utils/xh-utils/constant.ts
+++ src/utils/xh-utils/constant.ts
@@ -1,8 +1,42 @@
 interface constantObj {
     [key: string]: any
 }
+const time_constant: constantObj = {
+    SECOND: 1000,
+    "SECOND_1": 1000,
+    "SECOND_5": 5000,
+    "SECOND_10": 10000,
+    "SECOND_15": 15000,
+    "SECOND_30": 30000,
+    "MINUTE": 60000,
+    "MINUTE_1": 60000,
+    "MINUTE_5": 300000,
+    "MINUTE_10": 600000,
+    "MINUTE_15": 900000,
+    "MINUTE_30": 1800000,
+    "HOUR": 3600000,
+    "HOUR_1": 3600000,
+    "HOUR_5": 18000000,
+    "HOUR_10": 36000000,
+    "HOUR_15": 54000000,
+    "DAY": 86400000,
+    "DAY_1": 86400000,
+    "DAY_5": 432000000,
+    "DAY_10": 864000000,
+    "DAY_15": 1296000000,
+    "MONTH": 2592000000,
+    "MONTH_1": 2592000000,
+    "MONTH_5": 12460000000,
+    "MONTH_10": 25920000000,
+    "YEAR": 31536000000,
+    "YEAR_1": 31536000000,
+    "YEAR_5": 157760000000,
+    "YEAR_10": 315360000000,
+    "YEAR_100": 3153600000000,
+}
 const constant: constantObj = {
     "NOW_THEME": "__now_theme__",
+    ...time_constant
 } as const;
 
 export default constant;
--- src/utils/xh-utils/index.ts
+++ src/utils/xh-utils/index.ts
@@ -1,6 +1,7 @@
 import $store from './storage'
 import { parser, themeRegistry, getColor } from './style-utils/index'
 import { $nav } from './nav'
+import $constant from './constant'
 const $c = (...args: string[]) => parser(...args)
 $c.getColor = getColor
 $c.themeRegistry = themeRegistry
@@ -9,7 +10,8 @@ export {
     $store,
     $c,
     $nav,
-
+    $constant,
+    
     themeRegistry,
     getColor,
     parser
