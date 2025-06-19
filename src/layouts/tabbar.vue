252cb54ee43400dfe3937e68fd2dde863dd97809
我家猫会后空翻 <2194292088@qq.com>
2025-06-19 10:06:14 +0800

    feat(配置): 添加UniLayouts插件并创建tabbar布局
    添加@uni-helper/vite-plugin-uni-layouts插件以支持布局功能，并创建tabbar.vue布局文件用于统一管理底部导航栏样式

---
+++ src/layouts/tabbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <slot></slot>
+</template>
+<script setup lang="ts">
+import { $c } from '@/utils/xh-utils';
+import { onShow } from '@dcloudio/uni-app';
+onShow(() => {
+    uni.setTabBarStyle({
+        backgroundColor: $c.getColor('bg-base'),
+        color: $c.getColor('text-secondary'),
+        selectedColor: $c.getColor('text-base'),
+        borderStyle: $c.getCurrentTheme() == 'dark' ? 'white' : 'black',
+    })
+    uni.setNavigationBarColor({
+        frontColor: $c.getColor('status-light'),
+    })
+})
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import UniPages from '@uni-helper/vite-plugin-uni-pages'
+import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
 
 import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [UniPages(), uni()],
+  plugins: [UniPages(), UniLayouts(), uni()],
   resolve: {
     alias: {
       '@utils': resolve(__dirname, 'src/utils')
